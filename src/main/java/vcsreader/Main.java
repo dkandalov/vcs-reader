0eaa5903868cd41c4ecd2683a4bede5bfc9590fb
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-07-30 18:01:18 +0100

    Command class draft

--- src/main/java/vcsreader/Main.java
+++ src/main/java/vcsreader/Main.java
@@ -3,6 +3,7 @@ package vcsreader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 import static java.nio.charset.Charset.defaultCharset;
 
@@ -11,34 +12,69 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         String fromUrl = "https://github.com/junit-team/junit";
         String toFolder = "/tmp/junit-test";
-        new CommandRunner().runCommand("git", "clone", "-v", fromUrl, toFolder);
+        Command command = new Command("git", "clone", "-v", fromUrl, toFolder).execute();
+
+        System.out.println(command.stdout);
+        System.out.println(command.stderr);
+        System.out.println(command.exitValue);
     }
 
-    private static class CommandRunner {
-        public void runCommand(String... command) throws IOException, InterruptedException {
-            Process process = new ProcessBuilder(command).start();
-            BufferedReader stdoutReader = new BufferedReader(new InputStreamReader(process.getInputStream(), defaultCharset()));
-            BufferedReader stderrReader = new BufferedReader(new InputStreamReader(process.getErrorStream(), defaultCharset()));
+    private static class Command {
+        private final String[] command;
 
-            String stdout = "";
-            String stderr = "";
+        private String stdout = "";
+        private String stderr = "";
+        private int exitValue;
 
-            String s;
-            while ((s = stdoutReader.readLine()) != null) {
-                stdout += s;
+        private boolean finished;
+        private Exception exception;
+
+        private Command(String... command) {
+            this.command = command;
+        }
+
+        public Command execute() {
+            BufferedReader stdoutReader = null;
+            BufferedReader stderrReader = null;
+            try {
+                Process process = new ProcessBuilder(command).start();
+                stdoutReader = new BufferedReader(new InputStreamReader(process.getInputStream(), defaultCharset()));
+                stderrReader = new BufferedReader(new InputStreamReader(process.getErrorStream(), defaultCharset()));
+
+                String s;
+                while ((s = stdoutReader.readLine()) != null) {
+                    stdout += s;
+                }
+                while ((s = stderrReader.readLine()) != null) {
+                    stderr += s;
+                }
+
+                process.waitFor();
+                stdoutReader.close();
+                stderrReader.close();
+
+                process.destroy();
+                exitValue = process.exitValue();
+                finished = true;
+
+            } catch (IOException e) {
+                exception = e;
+            } catch (InterruptedException e) {
+                exception = e;
+            } finally {
+                close(stdoutReader);
+                close(stderrReader);
             }
-            while ((s = stderrReader.readLine()) != null) {
-                stderr += s;
+
+            return this;
+        }
+
+        private static void close(Reader reader) {
+            if (reader == null) return;
+            try {
+                reader.close();
+            } catch (IOException ignored) {
             }
-
-            System.out.println(stdout);
-            System.out.println(stderr);
-
-            process.waitFor();
-            stdoutReader.close();
-            stderrReader.close();
-
-            process.destroy();
         }
     }
 }
