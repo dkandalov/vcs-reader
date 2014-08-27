bbaefb2250f3a0148934a13c88b321557c1b93bf
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-08-27 08:26:07 +0100

    svn create test repo script

--- src/test/vcsreader/svn-create-repo.rb
+++ src/test/vcsreader/svn-create-repo.rb
@@ -25,66 +25,79 @@ class SvnRepositoryCreator
 
     Dir.chdir(@project_path) do
       puts `echo "file1 content" > file1.txt`
-      commit "initial commit", "2009-02-12T00:44:04.921324Z", @author
+      add "file1.txt"
+      commit "initial commit", "2014-08-10T15:00:00.000000Z", @author
 
-      # puts `echo "file2 content" > file2.txt`
-      # puts `echo "file3 content" > file3.txt`
-      # commit "added file2, file3", "Aug 11 15:00:00 2014 +0100", @author
-      #
-      # puts `echo "file2 new content" > file2.txt`
-      # puts `echo "file3 new content" > file3.txt`
-      # commit "modified file2, file3", "Aug 12 15:00:00 2014 +0100", @author
-      #
-      # puts `mkdir folder1`
-      # puts `mv file1.txt folder1/file1.txt`
-      # commit "moved file1", "Aug 13 15:00:00 2014 +0100", @author
-      #
-      # puts `mkdir folder2`
-      # puts `mv folder1/file1.txt folder2/renamed_file1.txt`
-      # commit "moved and renamed file1", "Aug 14 15:00:00 2014 +0100", @author
-      #
-      # puts `rm folder2/renamed_file1.txt`
-      # commit "deleted file1", "Aug 15 15:00:00 2014 +0100", @author
-      #
-      # @commit_hashes = log_commit_hashes
+      puts `echo "file2 content" > file2.txt`
+      puts `echo "file3 content" > file3.txt`
+      add "file2.txt", "file3.txt"
+      commit "added file2, file3", "2014-08-11T15:00:00.000000Z", @author
+
+      puts `echo "file2 new content" > file2.txt`
+      puts `echo "file3 new content" > file3.txt`
+      commit "modified file2, file3", "2014-08-12T15:00:00.000000Z", @author
+
+      mkdir "folder1"
+      move "file1.txt", "folder1/file1.txt"
+      commit "moved file1", "2014-08-13T15:00:00.000000Z", @author
+
+      mkdir "folder2"
+      move "folder1/file1.txt", "folder2/renamed_file1.txt"
+      commit "moved and renamed file1", "2014-08-14T15:00:00.000000Z", @author
+
+      rm "folder2/renamed_file1.txt"
+      commit "deleted file1", "2014-08-15T15:00:00.000000Z", @author
     end
 
-    # update_test_config(@commit_hashes)
+    update_test_config
   end
 
   private
 
   def create_dummy_hook(file_path)
     puts `echo "#!/bin/sh\nexit 0" > #{file_path}`
+    puts `chmod +x #{file_path}`
+  end
+
+  def add(*files)
+    files.each do |it|
+      puts `#{@svn} add #{it}`
+    end
+  end
+
+  def move(from, to)
+    puts `#{@svn} move #{from} #{to}`
+  end
+
+  def mkdir(dir)
+    puts `#{@svn} mkdir #{dir}`
+  end
+
+  def rm(file_name)
+    puts `#{@svn} rm #{file_name}`
   end
 
   def commit(message, date, author)
-    puts `#{@svn} add *`
     puts `#{@svn} commit -m "#{message}"`
     puts `#{@svn} propset svn:date --revprop -r HEAD #{date}`
+    puts `#{@svn} propset svn:author --revprop -r HEAD "#{author}"`
+    @revisions ||= 0
+    @revisions = @revisions + 1
+  end
+
+  def update_test_config
+    revisions_literal = (1..@revisions).to_a.join(",")
+    replace(
+        /"revisions": \[.*\]/,
+        "\"revisions\": [#{revisions_literal}]",
+        @path_to_config
+    )
   end
 
   def replace(pattern, replacement, file_name)
     text = File.read(file_name)
     text = text.gsub(pattern, replacement)
     File.open(file_name, "w") { |file| file.puts text }
-  end
-
-  def log_commit_hashes
-    log = `#{@svn} log`
-    log.split("\n").
-        delete_if { |line| not line.include?("commit ") }.
-        collect { |line| line.gsub(/commit /, "") }.
-        reverse
-  end
-
-  def update_test_config(commit_hashes)
-    hashes_literal = commit_hashes.collect { |hash| '"' + hash + '"' }.join(",")
-    replace(
-        /"revisions": \[.*\]/,
-        "\"revisions\": [#{hashes_literal}]",
-        @path_to_config
-    )
   end
 end
 
--- src/test/vcsreader/svn-log-playground.rb
+++ src/test/vcsreader/svn-log-playground.rb
@@ -1,6 +1,6 @@
 def svn_log
-  repo_url = "http://svn.apache.org/viewvc/commons/proper/collections/trunk"
-  date_range = "-r {2014-07-01}:{2014-08-01}"
+  repo_url = "file:///tmp/reference-repos/svn-repo"
+  date_range = "-r {2014-01-01}:{2015-01-01}"
   puts `svn log #{repo_url} #{date_range} --use-merge-history --stop-on-copy --verbose --xml`
 end
 
@@ -11,5 +11,5 @@ def svn_show_file_content
   puts `svn cat #{repo_url}/#{file}@#{revision}`
 end
 
-# svn_log
-svn_show_file_content
+svn_log
+# svn_show_file_content
