b6ec8c9ed140d301dbe1ef1a0186cd7b45505bd8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-06-15 23:15:37 +0100

    added back "nonExistentPath" field

--- src/test/org/vcsreader/vcs/git/GitIntegrationTestConfig.groovy
+++ src/test/org/vcsreader/vcs/git/GitIntegrationTestConfig.groovy
@@ -3,9 +3,10 @@ package org.vcsreader.vcs.git
 import org.vcsreader.lang.FileUtil
 
 class GitIntegrationTestConfig {
-	static String pathToGit = "/usr/local/Cellar/git/2.5.0/bin/git"
-	static String author = "Some Author"
-	static String authorWithEmail = "Some Author <some.author@mail.com>"
+	static final String pathToGit = "/usr/local/Cellar/git/2.5.0/bin/git"
+	static final String author = "Some Author"
+	static final String authorWithEmail = "Some Author <some.author@mail.com>"
+	static final String nonExistentPath = "/tmp/non-existent-path"
 
 	static String newReferenceRepoPath() {
 		def file = FileUtil.findSequentNonExistentFile(FileUtil.tempDirectoryFile(), "git-reference-repo-", "")
